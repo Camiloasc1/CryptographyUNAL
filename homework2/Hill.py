9764853059935164470f3aa3402d2bafe9e49af8
caasanchezcu <caasanchezcu@unal.edu.co>
2017-02-11 23:53:48 -0500

    Homework 2 - Hill Cipher now supports only valid keys

--- homework2/Hill.py
+++ homework2/Hill.py
@@ -38,7 +38,8 @@ def decrypt(cph, key, block_size):
         cph += 'X'
     cph = alpha_to_int(cph)
     key = np.array(key).reshape((block_size, block_size))
-    key = np.rint(inv(key) * det(key)).astype(int)  # Multiply by det(k) in order to be sure there are integers
+    _, x, _ = EEA(np.rint(det(key)).astype(int), 26)  # The modular inverse of det(k) % 26
+    key = np.rint(inv(key) * det(key) * x).astype(int)  # Multiply by det(k) in order get the adj(k)
     msg = []
     for c in zip(cph[::2], cph[1::2]):
         c = np.array(c)
@@ -46,8 +47,26 @@ def decrypt(cph, key, block_size):
     return int_to_alpha(msg)
 
 
+def EEA(a, b):
+    if b == 0:
+        return a, 1, 0
+    g, x, y = EEA(b, a % b)
+    return g, y, x - a // b * y
+
+
+def is_valid_key(key):
+    # from math import gcd
+
+    d = np.rint(det(np.array(key).reshape((2, 2)))).astype(int)
+    # return not (d == 0 or gcd(d, 26) != 1)  # this makes the key invalid for Hill cipher
+    return not (d == 0 or d % 2 == 0 or d % 13 == 0)  # this makes the key invalid for Hill cipher
+
+
 if __name__ == '__main__':
-    msg = sys.stdin.readline().strip()
+    msg = sys.stdin.readline().strip().upper()
     key = [int(n) for n in sys.stdin.readline().split()]
-    print("Encrypted: '" + encrypt(msg, key, 2) + "'")
-    print("Decrypted: '" + decrypt(msg, key, 2) + "'")
+    if is_valid_key(key):
+        print("Encrypted: '" + encrypt(msg, key, 2) + "'")
+        print("Decrypted: '" + decrypt(msg, key, 2) + "'")
+    else:
+        print("Invalid key")
--- homework2/test/test_Hill.py
+++ homework2/test/test_Hill.py
@@ -1,6 +1,6 @@
 import unittest
 
-from homework2.Hill import encrypt, decrypt
+from homework2.Hill import encrypt, decrypt, is_valid_key
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,6 +13,19 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual('NUMBERTHEORYISEASY', encrypt('VKFZRVWTIAZSMISGKA', [7, -8, -3, 11], 2))
         self.assertEqual('NUMBERTHEORYISEASY', decrypt('VKFZRVWTIAZSMISGKA', [11, 8, 3, 7], 2))
+
+        self.assertEqual('CMSF', encrypt('ASDF', [1, 2, 3, 5], 2))
+        self.assertEqual('ASDF', decrypt('CMSF', [1, 2, 3, 5], 2))
+
+        self.assertEqual('ASDF', encrypt('CMSF', [-5, 2, 3, -1], 2))
+        self.assertEqual('CMSF', decrypt('ASDF', [-5, 2, 3, -1], 2))
+
+        self.assertEqual(False, is_valid_key([1, 2, 3, 4]))
+        self.assertEqual(False, is_valid_key([1, 3, 5, 7]))
+        self.assertEqual(True, is_valid_key([11, 8, 3, 7]))
+        self.assertEqual(True, is_valid_key([7, -8, -3, 11]))
+        self.assertEqual(True, is_valid_key([1, 2, 3, 5]))
+        self.assertEqual(True, is_valid_key([-5, 2, 3, -1]))
 
 
 if __name__ == '__main__':
