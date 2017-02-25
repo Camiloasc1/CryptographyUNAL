87fce8b8bdb50a3037d8d74127a2925c1e05bc10
caasanchezcu <caasanchezcu@unal.edu.co>
2017-02-25 11:41:05 -0500

    Homework 4 - ImgDES

---
+++ homework4/ImgDES.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import base64
+from homework4.pyDes import *
+
+
+def encrypt(msg, key):
+    return des(key, CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5).encrypt(msg)
+
+
+def decrypt(cph, key):
+    return des(key, CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5).decrypt(cph, padmode=PAD_PKCS5)
+
+
+def read_file(path):
+    with open(path, mode='rb') as file:
+        data = file.read()
+    return data
+
+
+def write_file(path, data):
+    with open(path, mode='wb') as file:
+        file.write(data)
+
+
+if __name__ == '__main__':
+    print("Key: ", end='', flush=True)
+    key = sys.stdin.readline().strip()
+    if len(key) != 8:
+        print("Invalid DES key size. Key must be exactly 8 bytes long.")
+        exit()
+
+    print("Input file: ", end='', flush=True)
+    in_img = sys.stdin.readline().strip()
+    in_img = in_img if in_img else "in.png"
+
+    print("Output file: ", end='', flush=True)
+    out_img = sys.stdin.readline().strip()
+    out_img = out_img if out_img else "out.png"
+
+    msg = read_file(in_img)
+    cph = encrypt(msg, key)
+    cph64 = base64.standard_b64encode(cph)
+    print("Base64:", cph64)
+    cph = base64.standard_b64decode(cph64)
+    write_file(out_img, decrypt(cph, key))
+    os.system("xdg-open " + out_img)  # open file
